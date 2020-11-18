4b420c226335d9b3e9638bf1f038d8136451de53
astroboi-lab <terry007x@korea.ac.kr>
2020-11-18 15:24:23 +0900

    ToolLogics.checkSeqByChar

--- astroboi_bio_tools/ToolLogic.py
+++ astroboi_bio_tools/ToolLogic.py
@@ -1,3 +1,5 @@
+from Bio import pairwise2
+from Bio.SubsMat.MatrixInfo import blosum62
 
 
 class ToolLogics:
@@ -81,3 +83,29 @@ class ToolLogics:
             if self.match(0, long_seq[i: i + len_rule], rule_str):
                 idx_list.append(i)
         return idx_list
+
+    """
+    by using the BLOSUM62 matrix, together with a gap open penalty of 10 and a gap extension penalty of 0.5 (using globalds)
+    """
+    def get_pairwise2_globalds_result(self, asequence, bsequence, matrx=blosum62, gap_open_penalty=10, extension_penalty=0.5):
+        alignments = pairwise2.align.globalds(asequence.upper().replace(" ", ""), bsequence.upper().replace(" ", ""),
+                                              matrx, -gap_open_penalty, -extension_penalty)
+        alignments_result = pairwise2.format_alignment(*alignments[0])
+        align_arr = alignments_result.split("\n")
+        return align_arr[0], align_arr[1], align_arr[2], alignments_result
+
+    """
+    LSPADKTNVKAA
+      |..|..|
+    --PEEKSAV---
+    Score=16
+    <BLANKLINE>
+    """
+    def get_pairwise2_localds_result(self, asequence, bsequence, matrx=blosum62, gap_open_penalty=10, extension_penalty=1):
+        alignments = pairwise2.align.localds(asequence.upper().replace(" ", ""), bsequence.upper().replace(" ", ""),
+                                              matrx, -gap_open_penalty, -extension_penalty)
+        alignments_result = pairwise2.format_alignment(*alignments[0])
+        align_arr = alignments_result.split("\n")
+        return ''.join([i for i in align_arr[0] if not i.isdigit()]), ''.join(
+            [i for i in align_arr[1] if not i.isdigit()]), ''.join(
+            [i for i in align_arr[2] if not i.isdigit()]), alignments_result
